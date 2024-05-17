60b2ac579cb642d0e7aa7a1008dd355b9d14e065
root <Nathaniel.wilson@atlasschool.com>
2024-05-17 20:47:11 +0000

    fixed some issues with spacing and whatnot to satisfy betty

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function and entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+}
+putchar(' ');
+for (char c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
