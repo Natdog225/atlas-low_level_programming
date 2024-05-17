954e3865c69b8543a4e2c018ba125722daddaebf
root <Nathaniel.wilson@atlasschool.com>
2024-05-17 22:31:35 +0000

    added desc

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - entry point and function
+ * return - 0 always (successful)
+ */
 int main(void)
 {
-char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n";
+char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int i = 0;
 while (alphabet[i] != '\0')
