0029e38b0f41d8306c37bb491a140c96d56e213f
root <Nathaniel.wilson@atlasschool.com>
2024-05-17 18:21:32 +0000

    first attempt

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * Description: Is a fancy thing to tell what numbers are
+ * 
+ * main - main entry point for code
+ *
+ * Return (0) - successful
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+	
+	printf("Last digit of %d", n);
+
+       	if (n % 10 > 5)
+        {
+                printf("is greater than 5\n");
+        } else if (n % 10 == 0)
+        {
+                printf("is 0\n");
+        } else
+        {
+                printf("is less than 6 and not 0\n");
+        }
+        return (0);
+}
+
