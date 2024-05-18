ba729e5b99afb582ddc0217a7a1f814c0d8196cd
root <Nathaniel.wilson@atlasschool.com>
2024-05-18 00:29:09 +0000

    no errors first try so thats dope

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Idea is to print numbers
+ * Return: Always 0 (successful)
+ */
+int main()
+{
+	int i;
+	for(i = 0; i <= 9; i++)
+	{
+	putchar(i + '0');
+	}
+	putchar('\n');
+	return 0;
+}
