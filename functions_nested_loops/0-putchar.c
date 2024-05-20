44f8c982e3b2dcfeb86d98f8f561cfa01cb3736e
root <Nathaniel.wilson@atlasschool.com>
2024-05-20 22:18:58 +0000

    Attempt 1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
-
+#include "main.h"
+/**
+ * main - main entry point and function
+ * Function: puts the letters into the terminal
+ * Return: Always 0 (successful)
+ */
 int main(void)
 {
-	_putchar('_');
+        _putchar('_')
+        _putchar('p')
+        _putchar('u')
+        _putchar('t')
+        _putchar('c')
+        _putchar('h')
+        _putchar('a')
+        _putchar('r')
+        _putchar('\n')
 
-	_putchar('\n');
-	return (0);
+Return (0)
 }
+
