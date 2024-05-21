1e284cdb09fc64225b0c9f26f1af14169e9c1833
root <Nathaniel.wilson@atlasschool.com>
2024-05-21 00:11:55 +0000

    added comments for betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,10 @@
 #include "main.h"
-
+/**
+ * _islower - checks for lowercase
+ * @c: the char to check
+ * Return: 1 if c is lowercase, 0 if not
+ */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z')? 1 : 0;
+	return (c >= 'a' && c <= 'z' ? 1 : 0);
 }
