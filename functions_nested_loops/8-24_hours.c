5912f46c29027bbfe2a0feb49414468b4b392a51
root <Nathaniel.wilson@atlasschool.com>
2024-05-21 01:52:36 +0000

    Simplified it

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+/**
+ * jack_bauer - Prints every minute of the day in 24-hour format.
+ *
+ * Description: This function prints out the minutes and hours.
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
