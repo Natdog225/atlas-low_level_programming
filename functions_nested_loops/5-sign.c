420441c7384441243369006a46efe680900ed296
root <Nathaniel.wilson@atlasschool.com>
2024-05-21 00:52:16 +0000

    tried again

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * Description: Pos neg or not
- * print_sign: Tells ya if its pos, neg or not
+ * Description - Pos neg or not
+ * print_sign - Tells ya if its pos, neg or not
  * @n: is the int
  * Return: 1 if its greater, 0 if its 0, -1 if its less
  */
