6ea04b6c6e0e459f93b1ec6cacbdd067b8e30ab2
root <Nathaniel.wilson@atlasschool.com>
2024-05-21 00:44:26 +0000

    first

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign: Tells ya if its pos, neg or not
+ * @n: is the int
+ * Return: 1 if its greater, 0 if its 0, -1 if its less
+ */
+int print_sign(int n)
+{
+	if (n > 0) 
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
