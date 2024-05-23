216b140482e6b9ef117f47213d56941d2e113065
root <Nathaniel.wilson@atlasschool.com>
2024-05-23 20:42:59 +0000

    ideall somewhat easy

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * description: Multiplies two ints
+ *
+ * mul - multiplies
+ *
+ * @a: int no 1
+ *
+ * @b: int no 2
+ *
+ * Return: (a * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
