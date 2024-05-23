6112c9e7f04312b4954e14a34b8a2d41327fd72a
root <Nathaniel.wilson@atlasschool.com>
2024-05-23 19:15:33 +0000

    added docu

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * Description: Tells if a letter is uppercase
+ *
+ * _isupper - check desc
+ *
+ * @c - int used
+ */
 int _isupper(int c)
 {
 	return ((c >= 65 && c <= 90) ? 1 : 0);
