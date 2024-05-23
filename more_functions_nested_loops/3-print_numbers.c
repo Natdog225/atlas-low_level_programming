2ebdff26d60efb1fc1b3738edd4dd84da02c0d30
root <Nathaniel.wilson@atlasschool.com>
2024-05-23 21:31:27 +0000

    first

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+Include "main.h"
+/**
+ * description - it prints numbers
+ *
+ * print_numbers - this also prints numbers
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar ('\n');
+}
