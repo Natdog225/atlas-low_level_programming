57c1b5e2ab9b194846313ab54537071b14d25dae
root <Nathaniel.wilson@atlasschool.com>
2024-05-23 19:33:16 +0000

    first

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Description: Tells you if if the input is a digit
+ *
+ * isdigit - tells if its a digit
+ *
+ * @c: is the int
+ *
+ * Return: Always 0
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
