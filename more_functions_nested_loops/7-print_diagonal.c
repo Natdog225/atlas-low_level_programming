0bfd5f92491643ac69781d3007ee90492465e6fe
root <Nathaniel.wilson@atlasschool.com>
2024-05-24 00:14:07 +0000

    no betty errors first time? I might actually be getting this

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - It prints a line diagonally
+ *
+ * Description: Look at the function
+ *
+ * @n: the int used
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
