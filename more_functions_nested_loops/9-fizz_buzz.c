a7050d0cf7ce3e06fcec3e56b6c1a4ccb00807d7
root <Nathaniel.wilson@atlasschool.com>
2024-05-24 22:25:37 +0000

    added space after output

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,15 +6,15 @@ int main()
 	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
-		{printf("Fizzbuzz");
+		{printf("Fizzbuzz ");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
