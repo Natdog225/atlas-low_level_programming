2692875b013af2ee843d3945a02944cc9c159e1e
root <Nathaniel.wilson@atlasschool.com>
2024-05-27 22:55:35 +0000

    try again

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <main.h>
+/**
+ *
+ *
+ *
+ *
+ */
+void puts2(char *str)
+{
+	int len;
+	int x;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (x = 0; x < len; x += 2)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}
