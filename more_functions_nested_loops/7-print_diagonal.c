38104d481022a9a7984dd667923da47092117178
root <Nathaniel.wilson@atlasschool.com>
2024-05-28 22:01:24 +0000

    fixed issues with 7 and 9

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,18 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+        int i;
 
-	if (n <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
+        if (n <= 0)
+        {
+                _putchar('\n');
+                return;
+        }
 
-	for (i = 0; i < n; i++)
-	{
-		_putchar('\\');
-		_putchar(' ');
-	}
-	_putchar('\n');
+        for (i = 0; i < n; i++)
+        {
+                _putchar('\\');
+                _putchar(' ');
+        }
+        _putchar('\n');
 }
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-int main()
-{
-	int num;
+#include "main.h"
+/**
+ * main - main entry point and int
+ *
+ * num: thing in code
+ *
+ * return:0
+ */
+int main(void) {
+    int num;
 
-	for (num = 1; num <= 100; num++)
-	{
-		if (num % 3 == 0 && num % 5 == 0)
-		{printf("Fizzbuzz ");
-		}
-		else if (num % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (num % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", num);
-		}
-	}
-	printf("\n");
-	return (0);
+    for (num = 1; num <= 100; num++) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            printf("Fizzbuzz ");
+        } else if (num % 3 == 0) {
+            printf("Fizz ");
+        } else if (num % 5 == 0) {
+            printf("Buzz ");
+        } else {
+            printf("%d ", num);
+        }
+    }
+    printf("\n");
+    return (0);
 }
