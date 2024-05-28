fbcfc016ea6d3eab096dc7d9625acb63b39cb11b
root <Nathaniel.wilson@atlasschool.com>
2024-05-28 22:18:26 +0000

    attempt to fix it again

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,21 @@
  */
 void print_diagonal(int n)
 {
-        int i;
+	int i;
+	int x;
 
-        if (n <= 0)
-        {
-                _putchar('\n');
-                return;
-        }
-
-        for (i = 0; i < n; i++)
-        {
-                _putchar('\\');
-                _putchar(' ');
-        }
-        _putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (x = 0; x < i; x++)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
+	_putchar('\n');
+	}
 }
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,30 +8,28 @@
  */
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 1; num <= 100; num++)
-    {
-        if (num % 3 == 0 && num % 5 == 0)
-        {
-            printf("FizzBuzz");
-            if (num != 100)
-                printf(" ");
-        }
-        else if (num % 3 == 0)
-            printf("Fizz ");
-        else if (num % 5 == 0)
-        {
-            printf("Buzz");
-            if (num != 100)
-                printf(" ");
-        }
-        else
-            printf("%d ", num);
-    }
-
-    printf("\n");
-
-    return (0);
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+		printf("FizzBuzz");
+		if (num != 100)
+			printf(" ");
+		}
+	else if (num % 3 == 0)
+		printf("Fizz ");
+	else if (num % 5 == 0)
+	{
+		printf("Buzz");
+		if (num != 100)
+			printf(" ");
+	}
+	else
+		printf("%d ", num);
+	}
+	printf("\n");
+	return (0);
 }
 
