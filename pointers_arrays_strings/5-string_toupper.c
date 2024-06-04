a97d6117aae1db05c6661e9533ea6303b99b621e
root <Nathaniel.wilson@atlasschool.com>
2024-06-04 01:26:14 +0000

    I dont know

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+#include <stddef.h>
+/**
+ * string_toupper - converts all lowercase to upper
+ * @str: the string that changes
+ *
+ * Return: a pointer to the changed str
+ */
+char *string_toupper(char *str)
+{
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		*ptr = toupper((unsigned char)*ptr);
+		ptr++;
+	}
+	return (str);
+}
