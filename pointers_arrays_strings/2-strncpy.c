75504d075629df9c7bc434c1edbe78b9d59adc14
root <Nathaniel.wilson@atlasschool.com>
2024-06-04 03:05:27 +0000

    this is a commit

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -10,17 +10,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	size_t i;
-
+	int i;
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (i < n)
+	for ( ; i < n; i++)
 	{
-		dest[i++] = '\0';
+		dest[i] = '\0';
 	}
-
 	return (dest);
 }
-
