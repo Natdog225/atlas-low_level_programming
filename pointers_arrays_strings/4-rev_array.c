f9f4fab237aba0c110855d904a72a2df9edd7ea6
root <Nathaniel.wilson@atlasschool.com>
2024-06-04 01:12:26 +0000

    fixed betty and compile issues before push

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * reverse_array - Reverses the content of an array
+ * @a: the array to be reversed
+ * @n: number of things in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+	int temp;
+
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+	}
+}
+
