d339c1944443ed14070ff7677a4e7918a5dcd8df
root <Nathaniel.wilson@atlasschool.com>
2024-06-06 18:01:21 +0000

    Attempt no 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0' || *(s + 1) == '\0')
+	{
+		_putchar(*s);
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
