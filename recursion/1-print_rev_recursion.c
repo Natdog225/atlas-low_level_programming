8c7d628857212f2316fb95da6b4779aeb222c437
root <Nathaniel.wilson@atlasschool.com>
2024-06-06 20:12:14 +0000

    try no 4

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,11 +5,14 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
 	{
-		_putchar(*s);
-		return;
-	}
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+        if (*s == '\0' || *(s + 1) == '\0' || *(s + 2) == '\0') 
+	
+        {
+                _putchar(*s);
+                return;
+        }
+        _print_rev_recursion(s + 1);
+        _putchar(*s);
 }
+~
