aadf0534faf62ae23be0d7741de441a4d8e66b5f
root <Nathaniel.wilson@atlasschool.com>
2024-06-07 02:30:03 +0000

    try again

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,21 +7,21 @@
  * Return: -1, 0 then (i - 1) * (i - 1)
  */
 int _sqrt_recursion(int n)
-int i;
 {
+	int i;
+
 	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	if (n < 0)
-	{
-		return (-1);
-	}
+	return (n);
+	
+	else if (n < 0)
+	return (-1);
+	
 	i = 1;
 
 	while (i * i <= n)
-	{
-		i++;
-	}
-	return (i - 1) * (i - 1) == n ? (i - 1) : -1;
+	i++;
+	if ((i - 1) * (i - 1) == n)
+	return (i - 1);
+	else
+		return (-1);
 }
