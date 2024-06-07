d8ce5f685d3264eb2a37f84fccc781540cb9e161
root <Nathaniel.wilson@atlasschool.com>
2024-06-07 18:40:25 +0000

    Changed it all cause I had a loop in the last answer, now it seems way too long

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,20 +8,30 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if (n == 0 || n == 1)
-	return (n);
-	
-	else if (n < 0)
-	return (-1);
-	
-	i = 1;
-
-	while (i * i <= n)
-	i++;
-	if ((i - 1) * (i - 1) == n)
-	return (i - 1);
-	else
+	if (n < 0)
+	{
 		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		int root = _sqrt_recursion(n / 4) * 2;
+
+		if (root * root == n)
+		{
+			return (root);
+		}
+		else if ((root + 1) * (root + 1) == n)
+		{
+			return (root + 1);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
 }
+
