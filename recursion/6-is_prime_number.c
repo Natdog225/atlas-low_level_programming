e09e2867b54f50332eb2cbeb90f40005941a05c0
root <Nathaniel.wilson@atlasschool.com>
2024-06-07 19:05:43 +0000

    try no 1

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * is_prime_number - Checks if a int is a prime number.
+ *
+ * @n: the int
+ *
+ * Return: 1 if its a prime. 0 if not
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n / 2 == 0)
+	{
+		return (0);
+	}
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
