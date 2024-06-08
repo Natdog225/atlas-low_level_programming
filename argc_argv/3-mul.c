3520ac290b3873b3c05e3af83779f6131ec6c083
root <Nathaniel.wilson@atlasschool.com>
2024-06-08 02:41:13 +0000

    Since checker is down, Im just throwing things in for fun

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf ("Error\n");
+			return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 == 0 && argv [1] [0] != '0')
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (num2 == 0 && argv [2][0] != '0')
+	{
+		printf("Error\n");
+		return(1);
+	}
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
+
+
