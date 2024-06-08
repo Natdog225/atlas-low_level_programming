813ace7b754aaf06c249952cbcebfa98080a0ef0
root <Nathaniel.wilson@atlasschool.com>
2024-06-08 02:21:16 +0000

    too similar, Im doing something wrong

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Prints the number of args
+ * @argc: number of args
+ * @argv: Contains the args
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("d\n", argc - 1);
+	return (0);
+}
