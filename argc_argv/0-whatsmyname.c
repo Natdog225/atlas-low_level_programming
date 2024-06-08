0781e3bfceec2fa19675a6354541ea5772b198e0
root <Nathaniel.wilson@atlasschool.com>
2024-06-08 02:13:44 +0000

    Try no 1

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name followed by new line
+ * @argc: the number of args
+ * @argv: an array
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+This is about Argv and argc, prototypes included in the readme, most are pretty self explanitory
