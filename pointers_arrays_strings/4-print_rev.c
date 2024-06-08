e6f01209068ba69903a9938a0dac1e67a7fd8c56
root <Nathaniel.wilson@atlasschool.com>
2024-06-08 21:21:13 +0000

    I hate this

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
+ * print_rev - Reverses string
  *
+ * @len: Length of string 
  *
+ * @index: the string
  *
- *
- *
+ * Return: 0
  */
 void print_rev(char *s)
 {
@@ -17,21 +19,4 @@ void print_rev(char *s)
 	_putchar(s[index]);
 
 	_putchar('\n');
-}
-
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
- */
-int _strlen(char *s)
-{
-	int count;
-	int inc;
-
-	inc = 0;
-	for (count = 0; s[count] != '\0'; count++)
-	inc++;
-
-	return (inc);
 }
