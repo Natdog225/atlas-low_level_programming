fe0749b9e1fcb583daafe60d871dd9d653890648
root <Nathaniel.wilson@atlasschool.com>
2024-06-10 20:52:13 +0000

    Forgot to leave comments

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Multiplies two numbers.
+ * @argc: The number of arguments.
+ * @argv: An array of arguments.
+ *
+ * Return: 0 on success, 1 on error (incorrect number of arguments).
+ */
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
