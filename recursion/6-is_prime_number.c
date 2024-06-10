d266ff82bf4d79deb1f96019a694785abdbf6e3f
root <Nathaniel.wilson@atlasschool.com>
2024-06-10 22:09:04 +0000

    Well that didn't work at all, changed it around and added a helper function

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,26 +1,32 @@
 #include "main.h"
 #include <math.h>
 /**
- * is_prime_number - Checks if a int is a prime number.
+ * is_prime_number_helper - Checks if a int is a prime number.
  *
  * @n: the int
- *
+ * @divisor: Divisor
  * Return: 1 if its a prime. 0 if not
+ */
+int is_prime_number_helper(int n, int divisor)
+{
+	if (n <= 1 || n % divisor == 0)
+	{
+	return (0);
+	}
+	if(divisor * divisor > n)
+	{
+		return (1);
+	}
+	return (is_prime_number_helper(n, divisor + 1));
+}
+
+/**
+ * is_prime_number - does the same thing
+ * @n: num to check
+ *
+ * Return: 1 if prime, 0 if not
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+return is_prime_number_helper(n, 2);
 }
