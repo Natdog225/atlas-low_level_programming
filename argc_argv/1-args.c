078da9cd7bd79ce3a540d1e0b8f7b74d57f2b1bc
root <Nathaniel.wilson@atlasschool.com>
2024-06-10 20:19:14 +0000

    I am upset.

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,19 +3,11 @@
 /**
  * main - Prints the number of args
  * @argc: number of args
- * @argv: Contains the args
- * @i: int used
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc)
 {
-	int i;
 	printf("%d\n", argc);
-
-	for (i = 0; i < argc; i++)
-	{
-		printf ("%s\n", i, argv[i]);
-	}
 
 	return (0);
 }
