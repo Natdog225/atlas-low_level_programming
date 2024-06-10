1928205d7634b2448814bf190605ceef70c64d3c
root <Nathaniel.wilson@atlasschool.com>
2024-06-10 20:07:37 +0000

    Added something for argv so it let me compile

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,12 +4,19 @@
  * main - Prints the number of args
  * @argc: number of args
  * @argv: Contains the args
- *
+ * @i: int used
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
+	int i;
 	printf("Number of arguments: %d\n", argc);
+	printf("What's argv[]\n");
+
+	for (i = 0; i < argc; i++)
+	{
+		printf ("argv[%d] = %s\n", i, argv[i]);
+	}
 
 	return (0);
 }
