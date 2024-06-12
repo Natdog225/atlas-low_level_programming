4c9f06dfa97ec4a22167989419ad2ee55c68dd28
root <Nathaniel.wilson@atlasschool.com>
2024-06-12 22:56:07 +0000

    First attempt

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - Allocates memory. 
+ * @b: int used.
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This one is also about malloc and exit strategies and whatnot.
