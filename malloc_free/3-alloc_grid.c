cc06edd9842df9664c47a9d86d42c99035bc4994
root <Nathaniel.wilson@atlasschool.com>
2024-06-12 21:14:38 +0000

    No. 3 first attempt. Feels too wordy and I haven't tried to compile yet cause Im scared

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Allocates a 2D grid 
+ *
+ * @width: Width of ze grid.
+ * @height: Height of the grid
+ *
+ * Return: A pointer to the grid or NULL if it fails in its life's purpose
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int) * width);
+	if (grid[i] == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
+
