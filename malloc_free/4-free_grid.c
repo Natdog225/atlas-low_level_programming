e4c3155c4248b17aaa1b4212b904cf721c5fb8bc
root <Nathaniel.wilson@atlasschool.com>
2024-06-12 22:12:42 +0000

    Not sure what this is gonna do since I haven't figured out the memory issues yet with 3

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - Feel like this one is pretty self explanatory
+ * @grid: same thing
+ * @height: again pretty simple.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+	for (i = 0; i < heaight; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
