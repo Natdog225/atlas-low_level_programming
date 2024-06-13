a8e54f09a987e290d2f49e24e2c2d5314ebdb84a
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-13 14:01:45 -0500

    Formatted

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,17 +16,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     unsigned int i;
 
     if (s1 == NULL)
-    s1 = "";
+        s1 = "";
     if (s2 == NULL)
-    s2 = "";
-    
+        s2 = "";
+
     while (s1[len1] != '\0')
-    len1++;
+        len1++;
     while (s2[len2] != '\0')
-    len2++;
+        len2++;
 
     if (n > len2)
-    n = len2;
+        n = len2;
 
     result = malloc(len1 + n + 1);
     if (result == NULL)
@@ -34,12 +34,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
         exit(1);
     }
     for (i = 0; i < len1; i++)
-    result[i] = s1[i];
+        result[i] = s1[i];
 
     for (i = 0; i < n; i++)
-    result[len1 + i] = s2[i];
+        result[len1 + i] = s2[i];
 
-    result [len1 + n] = '\0';
-    
+    result[len1 + n] = '\0';
+
     return (result);
 }
