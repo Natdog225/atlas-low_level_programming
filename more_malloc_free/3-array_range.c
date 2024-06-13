3d8efd06a19afd5842ebdf1ddc937212c2cc284f
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-13 17:02:59 -0500

    First attempt at 3. I'm still not exactly sure how to use VScode but it is pretty nice.

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - Creates an array of ints
+ * @min: Minimum value
+ * @max: max value
+ * 
+ * Return: Pointer to the array, or NULL if min is greater than max or malloc fails
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i;
+	int len;
+
+	if (min > max)
+	return (NULL);
+
+	len = max - min + 1;
+	arr = malloc(sizeof(int) * len);
+
+	if (arr == NULL)
+	return (NULL);
+
+	for (i = 0; i < len; i++)
+	arr[i] = min + i;
+
+	return (arr);
+}
