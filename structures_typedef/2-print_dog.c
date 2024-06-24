b2767eead14c982f3675faef8a2d05c2ef50c319
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-24 02:06:36 -0500

    Attempt one with betty fixes ideally.

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	printf("Name: %s\nAge: %.1f\nOwner: %s\n",
+		   d->name ? d->name : "(nil)",
+		   d->age,
+		   d->owner ? d->owner : "(nil)");
+}
