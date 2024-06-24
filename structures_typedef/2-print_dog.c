4b43f32b80c580ad0ab7cf8699d387e35136e934
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-24 02:11:24 -0500

    Changed decimal count from 1 to 6

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,7 +8,7 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	printf("Name: %s\nAge: %.1f\nOwner: %s\n",
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n",
 		   d->name ? d->name : "(nil)",
 		   d->age,
 		   d->owner ? d->owner : "(nil)");
