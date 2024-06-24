fedde1a69764671013e1d0a544b4ed7c07972711
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-24 00:35:44 -0500

    Attempt 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#ifndef
+#define
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - initializes a variable
+ * @d: pointer to the struct dog
+ * @name: name to set
+ * @age: age to set
+ * @owner: owner to set
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
