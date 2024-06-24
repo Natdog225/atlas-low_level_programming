8c12e2cf5fbc72c2f0dc536f217f479b83833f52
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-24 01:05:11 -0500

    ....

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -16,22 +16,3 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 }
-
-#ifndef DOG_H
-#define DOG_H
-/**
- * struct dog - a struct representing a dog.
- * @name: name of the dog (%s)
- * @age: age of the dog (%f)
- * @owner: owner of the dog (%s)
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-#endif /* DOG_H */
