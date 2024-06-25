40d68ccd0e29af6a504c9151719a2b225fa78c35
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-24 23:51:10 -0500

    Formatted and added betty comments

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,15 @@
 #include "function_pointers.h"
 #include <stddef.h>
+/**
+ * print_name - Prints a name.
+ * @name: Pointer to the name
+ * @f: Pointer to the function used to print.
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 	{
-        f(name);
+		f(name);
 	}
 }
