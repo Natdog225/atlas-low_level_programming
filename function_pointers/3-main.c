238be6f4d42297ace5bff18bef5af9e2e91c2a75
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-25 16:25:38 -0500

    Added main to calc

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,3 +1,28 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL || (num2 == 0 && (*argv[2] == '/' || *argv[2] == '%')))
+	{
+		printf("Error\n");
+		exit((*argv[2] == '/' || *argv[2] == '%') ? 100 : 99);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
