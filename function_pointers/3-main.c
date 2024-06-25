1d6218ff5b6e66742023e54af3a0f7690ad52787
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-25 16:57:50 -0500

    Another attempt to fix error message for invalid chars

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -29,7 +29,8 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	else if ((num2 == 0) && ((*argv[2] == '/') || (*argv[2] == '%')))
+
+	if (((*argv[2] == '/') || (*argv[2] == '%')) && (num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
