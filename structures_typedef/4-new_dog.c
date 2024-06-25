c5965b54c1552a197b7f077a3dcd79a33ae6ea6f
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-24 22:54:43 -0500

    I've spent too much time on this one.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,7 +11,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i;
+	int i;
+	int name_len = 0;
+	int owner_len = 0;
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
@@ -19,9 +21,19 @@ int i;
 		return (NULL);
 	}
 
-	new_dog->name = malloc(strlen(name) + 1);
-	new_dog->owner = malloc(strlen(owner) + 1);
+	while (name[name_len] != '\0')
+	{
+		name_len++;
+	}
 
+	while (owner[owner_len] != '\0')
+	{
+		owner_len++;
+	}
+
+	new_dog->name = malloc(name_len + 1);
+	new_dog->owner = malloc(owner_len + 1);
+	
 	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
 		if (new_dog->name != NULL)
@@ -37,22 +49,22 @@ int i;
 	}
 
 	i = 0;
-while (name[i] != '\0') 
-{
-    new_dog->name[i] = name[i];
-    i++;
-}
-new_dog->name[i] = '\0'; 
+	while (name[i] != '\0')
+	{
+		new_dog->name[i] = name[i];
+		i++;
+	}
+	new_dog->name[i] = '\0';
 
-i = 0;
-while (owner[i] != '\0') 
-{
-    new_dog->owner[i] = owner[i];
-    i++;
-}
-new_dog->owner[i] = '\0';
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		new_dog->owner[i] = owner[i];
+		i++;
+	}
+	new_dog->owner[i] = '\0';
 
-new_dog->age = age;
+	new_dog->age = age;
 
 	return (new_dog);
 }
