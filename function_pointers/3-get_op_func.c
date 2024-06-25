de8b08e5e491b07b0546b9d984f5d6bed8c4fb03
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-25 17:04:15 -0500

    Formatted

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -7,22 +7,23 @@
  * Return: A pointer to the function
  */
 
-int (*get_op_func(char *s))(int, int) {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i = 0;
 
-    while (ops[i].op) {
-        if (*(ops[i].op) == *s && *(ops[i].op + 1) == '\0')
-            return (ops[i].f);
-        i++;
-    }
+	while (ops[i].op)
+	{
+		if (*(ops[i].op) == *s && *(ops[i].op + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
