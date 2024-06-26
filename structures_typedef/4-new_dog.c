2332d0c79451a69b7055c4e76a11aa405243950c
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-25 21:13:04 -0500

    appeased betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t));
 	if (!new_dog)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	new_dog->name = malloc(_strlen(name) + 1);
@@ -26,12 +26,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog->name);
 		free(new_dog->owner);
 		free(new_dog);
-		return NULL;
+		return (NULL);
 	}
 
 	_strcpy(new_dog->name, name);
 	_strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 
-	return new_dog;
+	return (new_dog);
 }
