72281fd377b9f702d4fdbc862f34b5f27730d4e6
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-06-25 20:39:55 -0500

    Fixed trailing whitespace

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,7 +11,7 @@ char *leet(char *str)
 	char *letters = "aAeEoOtTlL";
 	char *leet_chars = "4433007711";
 	int i, j;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; letters[j] != '\0'; j++)
