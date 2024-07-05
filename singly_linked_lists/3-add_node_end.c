e2c71f8a4eb176e4db6ff1eac3a5aa9752091a3f
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-05 10:26:35 -0500

    Attempt 2 on no 4.

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,6 +2,24 @@
 #include <string.h>
 
 /**
+ * _strlen - Calculates the length of a string.
+ * @str: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int length = 0;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+
+	return (length);
+}
+/**
  * add_node_end - Adds a new node at the end of a list.
  * @head: Pointer to a pointer to the head of the list.
  * @str: String to be duplicated and stored
@@ -25,7 +43,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(new_node->str);
+	new_node->len = _strlen(new_node->str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
