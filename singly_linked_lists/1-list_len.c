54c5b3d5a7b2c963b5b42b357d9c14c2f39851cb
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-05 09:59:06 -0500

    Attempt 1 on 2

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - Determines the number of elements in a linked list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of elements in the list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t element_count = 0;
+
+	/* Traverse the list until the end (NULL pointer) is reached */
+	while (h != NULL)
+	{
+		element_count++;
+		h = h->next;
+	}
+
+	return (element_count); /* Return the final count */
+}
