ae0803d86dafcbd1a0e4851ca2205da09fffd1a1
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-08 17:00:05 -0500

    no 4

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Adds a new node at the end of a list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: Integer data to store in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
