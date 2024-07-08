f111f2a3d109b954e715458f1f50be7391be824d
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-08 16:37:06 -0500

    fixed betty

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t element_count = 0;
+	size_t element_count = 0;
 
-    while (h != NULL)
-    {
-        element_count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		element_count++;
+		h = h->next;
+	}
 
-    return (element_count);
+	return (element_count);
 }
