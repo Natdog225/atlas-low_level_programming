b5e8d8abbb2887b447b4ea72b6fb2f1293df78ec
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-08 17:14:06 -0500

    Attempt no 2

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,4 +1,21 @@
+#include "lists.h"
 /**
  * free_listint2 - Frees a list and sets the head to NULL.
  * @head: Pointer to a pointer to the head of the list.
  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+
+	*head = NULL;
+}
