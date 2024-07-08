9ede02081059f1dc4e1cd6cc3376d3f59a86970f
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-08 17:18:03 -0500

    no 7 no 1

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+/**
+ * pop_listint - Deletes the head node of a list and returns its data.
+ * @head: Pointer to a pointer to the head of the list.
+ *
+ * Return: The data of the head node, or 0 if the list is empty.
+ */
