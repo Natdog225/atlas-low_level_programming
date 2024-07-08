2ecb4f3bc963d3bcf4e95b8c3437839e05f19bd3
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-08 16:41:37 -0500

    No 3 first draft, just betty notes

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,7 @@
+/**
+ * add_nodeint - Adds a new node at the beginning of a list.
+ * @head: Pointer to a pointer to the head of the list.
+ * @n: Integer data to store in the new node
+ *
+ * Return: The address of new element or null if failed. 
+ */
