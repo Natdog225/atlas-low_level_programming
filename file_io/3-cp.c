dffb9e8f5f136663b09116485d377420ba4edfb6
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-09 23:43:42 -0500

    Next bit of no 4

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,3 +37,21 @@ int main(int argc, char *argv[])
 		close(source_fd);
 		exit(99);
 	}
+	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(dest_fd, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(source_fd);
+			close(dest_fd);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(source_fd);
+		close(dest_fd);
+		exit(98);
+	}
