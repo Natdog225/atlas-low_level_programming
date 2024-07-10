31b14c6e9da2f8615d365541c66d1dbe044e13c1
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-09 23:06:35 -0500

    update

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * read_textfile - Reads a text file and prints it
+ * @filename: The name of the file to be read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 if an error occurs.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -8,19 +14,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	/* Handle NULL filename or zero letters */
 	if (filename == NULL || letters == 0)
-		return 0;
+		return (0);
 
 	/* Open the file for reading */
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return 0;
+		return (0);
 
 	/* Allocate buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
-		return 0;
+		return (0);
 	}
 
 	/* Read from the file into the buffer */
@@ -29,7 +35,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		close(fd);
-		return 0;
+		return (0);
 	}
 
 	/* Write to standard output */
@@ -38,10 +44,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		close(fd);
-		return 0;
+		return (0);
 	}
 
 	free(buffer);
 	close(fd);
-	return bytes_written;
+	return (bytes_written);
 }
