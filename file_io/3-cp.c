10fa307049d967127820802157953ddb9f0d4fde
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-09 23:32:31 -0500

    First few bits for no 4

---
+++ file_io/3-cp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 on success, 97, 98, 99, or 100 on specific errors.
+ */
+int main(int argc, char *argv[])
+{
+    int source_fd, dest_fd;
+    ssize_t bytes_read, bytes_written;
+    char buffer[BUFFER_SIZE];
+
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
