b63bdd40b426b05569543f397e57cad40c68418c
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-09 23:02:13 -0500

    update main

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> 
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
