78e5dccaadb6f9b1cafd65aca934d52d3778a283
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-09 23:07:49 -0500

    attempting to appease betty

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * @filename: The name of the file to be read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters read and printed, or 0 if an error occurs.
+ * Return: The actual number of letters read and printed, or 0 if an error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -46,7 +46,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	free(buffer);
 	close(fd);
 	return (bytes_written);
