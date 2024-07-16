eb8ccb6d9c5c2d14a91628dc3ef7f98a8979dcc9
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-15 22:07:06 -0500

    Attempting to make it work.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,24 +1,56 @@
 #include "hash_tables.h"
-#include <string.h>
 
 /**
  * hash_table_set - Adds an element to the hash table.
  * @ht: The hash table you want to add or update the key/value to
  * @key: The key. key can not be an empty string
- * @value: The value associated with the key. value must be duplicated. value can be an empty string
+ * @value: The value associated with the key. value must be duplicated.
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new_node;
-    char *value_copy;
-    unsigned long int index, i;
+	hash_node_t *new_node;
+	char *value_copy;
+	unsigned long int index, i;
 
-    /* Parameter validation */
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (0);
+	/* Parameter validation */
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
 
-    value_copy = strdup(value);
-    if (value_copy == NULL)
-        return (0);
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+	/* Find the index of the key using djb2 hash and key_index functions */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	/* Check if the key already exists */
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		free(value_copy);
+		return (0);
+	}
+	new_node->value = value_copy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
