c6d6b9d00aeb184b327862a176208f1a964ad34b
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-15 20:17:08 -0500

    updated lists

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -3,6 +3,8 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
