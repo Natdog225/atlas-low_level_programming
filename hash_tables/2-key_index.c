64d7d70c7ba31041e6e207d7b9f081fa1b173a97
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-15 21:54:06 -0500

    Removed some words

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  * @key: The key.
  * @size: The size of the array of the hash table.
  *
- * Return: The index at which the key/value pair should be stored in the hash table.
+ * Return: The index at which the key/value pair should be stored
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
