4146139d846c145a09f8a0d64920c271b9ffe5b8
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-15 22:23:19 -0500

    Here's hoping.

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: The hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	char comma_flag = 0; /* Flag to hopefully see if comma is needed */
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (comma_flag)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			comma_flag = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
