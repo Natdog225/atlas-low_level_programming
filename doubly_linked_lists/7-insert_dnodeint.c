ff68a96ae6fd419556b032dae17247cb9a9623eb
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-07-15 20:55:17 -0500

    Betty fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -6,7 +6,7 @@
  * @idx: Index at which the new node should be added, starting from 0.
  * @n: Data for the new node.
  *
- * Return: The address of the new node, or NULL if it failed or the index is out of bounds.
+ * Return: The address of the new node, or NULL if it failed or the index is OOB
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
